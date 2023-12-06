43d814c2feadb9e572ed3f28a8cac7740b574436
Ty Garside <tyrone.garside@dcsg.com>
2023-12-06 11:47:51 -0500

    Day04 - Complete

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,11 +6,6 @@ fun main() {
         val id: Int,
         val winning: Set<Int>,
         val picks: Set<Int>,
-        val score: Int = 2.0.pow(
-            picks.count {
-                winning.contains(it)
-            } - 1.0
-        ).toInt()
     )
 
     fun parseCard(line: String) = Card(
@@ -19,20 +14,41 @@ fun main() {
         line.substringAfter(": ").substringAfter(" | ").trim().split(Regex(" +")).map { it.toInt() }.toSet(),
     )
 
+    fun Card.score() = 2.0.pow(
+        picks.count {
+            winning.contains(it)
+        } - 1.0
+    ).toInt()
+
     fun part1(input: List<String>): Int {
-        return input.map { parseCard(it) }.onEach { println(it) }.sumOf { it.score }
+        return input.map { parseCard(it) }
+//            .onEach { println(it) }
+            .sumOf { it.score() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val cardCounts = object : MutableMap<Int, Int> by mutableMapOf() {
+            fun increment(key: Int) = this.compute(key) { _, value -> (value ?: 0) + 1 } ?: 0
+        }
+
+        for (card in input.map { parseCard(it) }) {
+            val winCount = card.picks.count { card.winning.contains(it) }
+            repeat(cardCounts.increment(card.id)) {
+                for (id in card.id + 1..card.id + winCount) {
+                    cardCounts.increment(id)
+                }
+            }
+        }
+
+        return cardCounts.values.sum()
     }
 
-// test if implementation meets criteria from the description, like:
+    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
